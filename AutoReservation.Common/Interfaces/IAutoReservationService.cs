4c228e74216512e9341bcf0f39fd47feb860c009
Nico Vinzens <Nico Vinzens>
2016-11-30 14:41:07 +0100

    please no conflict

--- AutoReservation.Common/Interfaces/IAutoReservationService.cs
+++ AutoReservation.Common/Interfaces/IAutoReservationService.cs
@@ -1,8 +1,20 @@
 ﻿using AutoReservation.Common.DataTransferObjects;
+using System.Collections.Generic;
 
 namespace AutoReservation.Common.Interfaces
 {
     public interface IAutoReservationService
     {
+        IList<AutoDto> GetAutos();
+        AutoDto GetAutoById(int Id);
+        void InsertAuto(AutoDto auto);
+        void UpdateAuto(AutoDto auto);
+        void DeleteAuto(AutoDto auto);
+
+        IList<KundeDto> GetKunden();
+        KundeDto GetKundeById(int Id);
+        void InsertKunde(KundeDto kunde);
+        void UpdateKunde(KundeDto kunde);
+        void DeleteKunde(KundeDto kunde);
     }
 }
--- AutoReservation.Service.Wcf/AutoReservationService.cs
+++ AutoReservation.Service.Wcf/AutoReservationService.cs
@@ -1,14 +1,33 @@
-﻿using System;
+﻿using AutoReservation.Common.Interfaces;
+using System;
 using System.Diagnostics;
+using AutoReservation.Common.DataTransferObjects;
+using System.Collections.Generic;
+using AutoReservation.Dal;
+using AutoReservation.Dal.Entities;
 
-namespace AutoReservation.Service.Wcf
-{
-    public class AutoReservationService
-    {
+namespace AutoReservation.Service.Wcf {
+    public class AutoReservationService : IAutoReservationService {
 
-        private static void WriteActualMethod()
-        {
+        private static void WriteActualMethod() {
             Console.WriteLine($"Calling: {new StackTrace().GetFrame(1).GetMethod().Name}");
+        }
+
+        public AutoDto GetAutoById(int Id) {
+            using (var db = new AutoReservationContext()) {
+                AutoDto auto = db.Autos.Find(Id).ConvertToDto();
+                if (auto != null) {
+                    return auto;
+                } else //FaultException {
+                    throw new Exception();
+            }
+        }
+
+        public IList<AutoDto> GetAutos() {
+            using (var db = new AutoReservationContext()) {
+                List<AutoDto> list = db.Autos.ConvertToDtos();
+                return list;
+            }
         }
     }
 }
