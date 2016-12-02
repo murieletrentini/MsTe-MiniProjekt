9554c8f5988e9675d078fbea04c9545f0d72ff52
Nico Vinzens <Nico Vinzens>
2016-12-02 12:19:17 +0100

    minor commit related to WCF Service, still not sure how to implement Exceptions / Faultcontracsts and if Operationcontracts are needed for service

--- AutoReservation.Common/Interfaces/IAutoReservationService.cs
+++ AutoReservation.Common/Interfaces/IAutoReservationService.cs
@@ -1,26 +1,46 @@
 ﻿using AutoReservation.Common.DataTransferObjects;
 using System.Collections.Generic;
+using System.ServiceModel;
 
 namespace AutoReservation.Common.Interfaces
 {
+    [ServiceContract]
     public interface IAutoReservationService
     {
+        [OperationContract]
         IList<AutoDto> GetAutos();
+        [OperationContract]
         AutoDto GetAutoById(int Id);
+        [OperationContract]
         void InsertAuto(AutoDto auto);
+        [OperationContract]
+        [FaultContract(typeof(LocalOptimisticConcourrencyException))]
         void UpdateAuto(AutoDto auto);
+        [OperationContract]
         void DeleteAuto(AutoDto auto);
 
+        [OperationContract]
         IList<KundeDto> GetKunden();
+        [OperationContract]
         KundeDto GetKundeById(int Id);
+        [OperationContract]
         void InsertKunde(KundeDto kunde);
+        [OperationContract]
+        [FaultContract(typeof(LocalOptimisticConcourrencyException))]
         void UpdateKunde(KundeDto kunde);
+        [OperationContract]
         void DeleteKunde(KundeDto kunde);
 
+        [OperationContract]
         IList<ReservationDto> GetReservationen();
+        [OperationContract]
         ReservationDto GetReservationById(int Id);
+        [OperationContract]
         void InsertReservation(ReservationDto reservation);
+        [OperationContract]
+        [FaultContract(typeof(LocalOptimisticConcourrencyException))]
         void UpdateReservation(ReservationDto reservation);
+        [OperationContract]
         void DeleteReservation(ReservationDto reservation);
     }
 }
